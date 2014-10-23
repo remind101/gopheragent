17c3ea626a296b93c867b1dff9c35adfc8aa613c
Nathan Yergler <nathan@yergler.net>
2014-10-23 14:37:13 -0700

    Port user_agent library to golang.

---
+++ useragent.go
@@ -0,0 +1,427 @@
+package useragent
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+// Browsers
+const (
+	Konqueror   = "konqueror"
+	Chrome      = "chrome"
+	Safari      = "safari"
+	Opera       = "opera"
+	PS3         = "ps3"
+	PSP         = "psp"
+	Firefox     = "firefox"
+	Lotus       = "lotus"
+	Netscape    = "netscape"
+	SeaMonkey   = "seamonkey"
+	Thunderbird = "thunderbird"
+	Outlook     = "outlook"
+	Evolution   = "evolution"
+	IEMobile    = "iemobile"
+	IE          = "ie"
+)
+
+// Engines
+const (
+	Webkit = "webkit"
+	Khtml  = "khtml"
+	Presto = "presto"
+	Gecko  = "gecko"
+	Msie   = "msie"
+)
+
+// Platforms
+const (
+	Windows      = "windows"
+	Mac          = "macintosh"
+	Linux        = "linux"
+	Wii          = "wii"
+	Playstation  = "playstation"
+	Ipad         = "ipad"
+	Ipod         = "ipod"
+	Iphone       = "iphone"
+	Android      = "android"
+	Blackberry   = "blackberry"
+	WindowsPhone = "windows_phone"
+	Symbian      = "symbian"
+)
+
+// Unknown is returned when a result cannot be extracted
+const Unknown = "unknown"
+
+type regexpTest struct {
+	Result  string
+	Pattern *regexp.Regexp
+	Expand  bool
+}
+
+var browsers,
+	engines,
+	oses,
+	platforms []regexpTest
+var browserVersions map[string]*regexp.Regexp
+var mobilePlatforms []string
+
+// UserAgent provides methods for extracting UA details
+type UserAgent struct {
+	s string
+}
+
+// New returns a UserAgent for the given UA string
+func New(ua string) *UserAgent {
+
+	result := UserAgent{
+		s: strings.TrimSpace(ua),
+	}
+
+	return &result
+}
+
+// BrowserName returns the name of the browser from the user agent
+func (ua *UserAgent) BrowserName() string {
+
+	return matchFirst(browsers, ua.s)
+
+}
+
+// BrowserVersion returns the version of the browser from the user agent
+func (ua *UserAgent) BrowserVersion() string {
+
+	r, err := browserVersionRegexp(ua.BrowserName())
+	if err == nil {
+		matches := r.FindStringSubmatch(ua.s)
+
+		if len(matches) > 1 {
+			return matches[1]
+		}
+	}
+
+	return ""
+}
+
+// Engine returns the rendering engine from the user agent
+func (ua *UserAgent) Engine() string {
+
+	return matchFirst(engines, ua.s)
+
+}
+
+// EngineVersion returns the version of the rendering engine used
+func (ua *UserAgent) EngineVersion() string {
+
+	engineVersion, err := regexp.Compile(`(?i:` + ua.Engine() + `[\/ ]([\d\w\.\-]+))`)
+
+	if err == nil {
+		matches := engineVersion.FindStringSubmatch(ua.s)
+
+		if len(matches) > 1 {
+			return matches[1]
+		}
+
+	}
+
+	return ""
+
+}
+
+// OS returns the operating system from the user agent
+func (ua *UserAgent) OS() string {
+
+	return matchFirst(oses, ua.s)
+
+}
+
+// Platform returns the platform from the user agent
+func (ua *UserAgent) Platform() string {
+
+	return matchFirst(platforms, ua.s)
+
+}
+
+// Mobile returns true if the user agent represents a mobile client
+func (ua *UserAgent) Mobile() bool {
+
+	platform := ua.Platform()
+
+	for _, t := range mobilePlatforms {
+		if t == platform {
+			return true
+		}
+	}
+
+	return ua.BrowserName() == PSP
+
+}
+
+func browserVersionRegexp(b string) (r *regexp.Regexp, err error) {
+
+	r, ok := browserVersions[b]
+	if !ok {
+		r, err = regexp.Compile(
+			`(?i:` + b + `[\/ ]([\d\w\.\-]+)`,
+		)
+	}
+
+	return r, err
+}
+
+func matchFirst(pp []regexpTest, ua string) string {
+
+	for _, test := range pp {
+		if m := test.Pattern.FindStringSubmatch(ua); m != nil {
+
+			// see if we need to expand the result
+			if test.Expand && len(m) > 1 {
+				substrings := make([]interface{}, len(m)-1)
+				for i := 1; i < len(m); i++ {
+					substrings[i-1] = interface{}(m[i])
+				}
+				return fmt.Sprintf(test.Result, substrings...)
+			}
+
+			return test.Result
+		}
+	}
+
+	return Unknown
+}
+
+func init() {
+
+	browsers = []regexpTest{
+		regexpTest{
+			Result:  Konqueror,
+			Pattern: regexp.MustCompile(`(?i:konqueror)`),
+		},
+		regexpTest{
+			Result:  Chrome,
+			Pattern: regexp.MustCompile(`(?i:chrome)`),
+		},
+		regexpTest{
+			Result:  Safari,
+			Pattern: regexp.MustCompile(`(?i:safari)`),
+		},
+		regexpTest{
+			Result:  IE,
+			Pattern: regexp.MustCompile(`(?i:msie)`)},
+		regexpTest{
+			Result:  Opera,
+			Pattern: regexp.MustCompile(`(?i:opera)`),
+		},
+		regexpTest{
+			Result:  PS3,
+			Pattern: regexp.MustCompile(`(?i:playstation 3)`),
+		},
+		regexpTest{
+			Result:  PSP,
+			Pattern: regexp.MustCompile(`(?i:playstation portable)`),
+		},
+		regexpTest{
+			Result:  Firefox,
+			Pattern: regexp.MustCompile(`(?i:firefox)`),
+		},
+		regexpTest{
+			Result:  Lotus,
+			Pattern: regexp.MustCompile(`(?i:lotus.notes)`),
+		},
+		regexpTest{
+			Result:  Netscape,
+			Pattern: regexp.MustCompile(`(?i:netscape)`),
+		},
+		regexpTest{
+			Result:  SeaMonkey,
+			Pattern: regexp.MustCompile(`(?i:seamonkey)`),
+		},
+		regexpTest{
+			Result:  Thunderbird,
+			Pattern: regexp.MustCompile(`(?i:thunderbird)`),
+		},
+		regexpTest{
+			Result:  Outlook,
+			Pattern: regexp.MustCompile(`(?i:microsoft.outlook)`),
+		},
+		regexpTest{
+			Result:  Evolution,
+			Pattern: regexp.MustCompile(`(?i:evolution)`),
+		},
+		regexpTest{
+			Result:  IEMobile,
+			Pattern: regexp.MustCompile(`(?i:iemobile|windows phone)`),
+		},
+	}
+
+	browserVersions = map[string]*regexp.Regexp{
+		Chrome: regexp.MustCompile(`(?i:chrome\/([\d\w\.\-]+))`),
+		Safari: regexp.MustCompile(`(?i:version\/([\d\w\.\-]+))`),
+		PS3:    regexp.MustCompile(`(?i:([\d\w\.\-]+)\)\s*$)`),
+		PSP:    regexp.MustCompile(`(?i:([\d\w\.\-]+)\)?\s*$)`),
+		Lotus:  regexp.MustCompile(`(?i:Lotus-Notes\/([\w.]+))`),
+	}
+
+	engines = []regexpTest{
+		regexpTest{
+			Result:  Webkit,
+			Pattern: regexp.MustCompile(`(?i:webkit)`),
+		},
+
+		regexpTest{
+			Result:  Khtml,
+			Pattern: regexp.MustCompile(`(?i:khtml)`),
+		},
+		regexpTest{
+			Result:  Konqueror,
+			Pattern: regexp.MustCompile(`(?i:konqueror)`),
+		},
+		regexpTest{
+			Result:  Chrome,
+			Pattern: regexp.MustCompile(`(?i:chrome)`),
+		},
+		regexpTest{
+			Result:  Presto,
+			Pattern: regexp.MustCompile(`(?i:presto)`),
+		},
+		regexpTest{
+			Result:  Gecko,
+			Pattern: regexp.MustCompile(`(?i:gecko)`),
+		},
+		regexpTest{
+			Result:  Unknown,
+			Pattern: regexp.MustCompile(`(?i:opera)`),
+		},
+		regexpTest{
+			Result:  Msie,
+			Pattern: regexp.MustCompile(`(?i:msie)`),
+		},
+	}
+
+	oses = []regexpTest{
+		regexpTest{
+			Result:  "Windows Phone",
+			Pattern: regexp.MustCompile(`(?i:windows (ce|phone|mobile)( os)?)`),
+		},
+		regexpTest{
+			Result:  "Windows Vista",
+			Pattern: regexp.MustCompile(`(?i:windows nt 6\.0)`),
+		},
+		regexpTest{
+			Result:  "Windows 7",
+			Pattern: regexp.MustCompile(`(?i:windows nt 6\.\d+)`),
+		},
+		regexpTest{
+			Result:  "Windows 2003",
+			Pattern: regexp.MustCompile(`(?i:windows nt 5\.2)`),
+		},
+		regexpTest{
+			Result:  "Windows XP",
+			Pattern: regexp.MustCompile(`(?i:windows nt 5\.1)`),
+		},
+		regexpTest{
+			Result:  "Windows 2000",
+			Pattern: regexp.MustCompile(`(?i:windows nt 5\.0)`),
+		},
+		regexpTest{
+			Result:  "Windows",
+			Pattern: regexp.MustCompile(`(?i:windows)`),
+		},
+		regexpTest{
+			Result:  "OS X %s.%s",
+			Pattern: regexp.MustCompile(`(?i:os x (\d+)[._](\d+))`),
+			Expand:  true,
+		},
+		regexpTest{
+			Result:  "Linux",
+			Pattern: regexp.MustCompile(`(?i:linux)`),
+		},
+		regexpTest{
+			Result:  "Wii",
+			Pattern: regexp.MustCompile(`(?i:wii)`),
+		},
+		regexpTest{
+			Result:  "Playstation",
+			Pattern: regexp.MustCompile(`(?i:playstation 3)`),
+		},
+		regexpTest{
+			Result:  "Playstation",
+			Pattern: regexp.MustCompile(`(?i:playstation portable)`),
+		},
+		regexpTest{
+			Result:  "iPad OS %s.%s",
+			Pattern: regexp.MustCompile(`(?i:\(iPad.*os (\d+)[._](\d+))`),
+			Expand:  true,
+		},
+		regexpTest{
+			Result:  "iPhone OS %s.%s",
+			Pattern: regexp.MustCompile(`(?i:\(iPhone.*os (\d+)[._](\d+))`),
+			Expand:  true,
+		},
+		regexpTest{
+			Result:  "Symbian OS",
+			Pattern: regexp.MustCompile(`(?i:symbian(os)?)`),
+		},
+	}
+
+	platforms = []regexpTest{
+		regexpTest{
+			Result:  WindowsPhone,
+			Pattern: regexp.MustCompile(`(?i:windows (ce|phone|mobile)( os)?)`),
+		},
+		regexpTest{
+			Result:  Windows,
+			Pattern: regexp.MustCompile(`(?i:windows)`),
+		},
+		regexpTest{
+			Result:  Mac,
+			Pattern: regexp.MustCompile(`(?i:macintosh)`),
+		},
+		regexpTest{
+			Result:  Android,
+			Pattern: regexp.MustCompile(`(?i:android)`),
+		},
+		regexpTest{
+			Result:  Blackberry,
+			Pattern: regexp.MustCompile(`(?i:blackberry)`),
+		},
+		regexpTest{
+			Result:  Linux,
+			Pattern: regexp.MustCompile(`(?i:linux)`),
+		},
+		regexpTest{
+			Result:  Wii,
+			Pattern: regexp.MustCompile(`(?i:wii)`),
+		},
+		regexpTest{
+			Result:  Playstation,
+			Pattern: regexp.MustCompile(`(?i:playstation)`),
+		},
+		regexpTest{
+			Result:  Ipad,
+			Pattern: regexp.MustCompile(`(?i:ipad)`),
+		},
+		regexpTest{
+			Result:  Ipod,
+			Pattern: regexp.MustCompile(`(?i:ipod)`),
+		},
+		regexpTest{
+			Result:  Iphone,
+			Pattern: regexp.MustCompile(`(?i:iphone)`),
+		},
+		regexpTest{
+			Result:  Symbian,
+			Pattern: regexp.MustCompile(`(?i:symbian(os)?)`),
+		},
+	}
+
+	mobilePlatforms = []string{
+		Android,
+		Blackberry,
+		Ipad,
+		Ipod,
+		Iphone,
+		Symbian,
+		WindowsPhone,
+	}
+}
---
+++ useragent_test.go
@@ -0,0 +1,202 @@
+package useragent_test
+
+import (
+	"testing"
+
+	"."
+)
+
+type UserAgentTestCase struct {
+	UA,
+	BrowserName,
+	BrowserVersion,
+	Engine,
+	EngineVersion,
+	OS,
+	Platform string
+	Mobile bool
+}
+
+func Test_UserAgent_Parse(t *testing.T) {
+
+	testCases := []UserAgentTestCase{
+
+		UserAgentTestCase{
+			UA:             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/6.1.3 Safari/537.75.14",
+			BrowserName:    "safari",
+			BrowserVersion: "6.1.3",
+			Engine:         "webkit",
+			EngineVersion:  "537.75.14",
+			OS:             "OS X 10.8",
+			Platform:       "macintosh",
+			Mobile:         false,
+		},
+
+		UserAgentTestCase{
+			UA:             "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36",
+			BrowserName:    "chrome",
+			BrowserVersion: "36.0.1985.143",
+			Engine:         "webkit",
+			EngineVersion:  "537.36",
+			OS:             "Windows 7",
+			Platform:       "windows",
+			Mobile:         false,
+		},
+
+		UserAgentTestCase{
+			UA:             "Mozilla/5.0 (Linux; U; Android 4.1.2; es-us; SGH-T599N Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+			BrowserName:    "safari",
+			BrowserVersion: "4.0",
+			Engine:         "webkit",
+			EngineVersion:  "534.30",
+			OS:             "Linux",
+			Platform:       "android",
+			Mobile:         true,
+		},
+
+		UserAgentTestCase{
+			UA:             "Mozilla/5.0 (Linux; U; Android 4.1.2; en-us; SGH-T999 Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+			BrowserName:    "safari",
+			BrowserVersion: "4.0",
+			Engine:         "webkit",
+			EngineVersion:  "534.30",
+			OS:             "Linux",
+			Platform:       "android",
+			Mobile:         true,
+		},
+
+		UserAgentTestCase{
+			UA:             "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36",
+			BrowserName:    "chrome",
+			BrowserVersion: "36.0.1985.125",
+			Engine:         "webkit",
+			EngineVersion:  "537.36",
+			OS:             "Windows 7",
+			Platform:       "windows",
+			Mobile:         false,
+		},
+
+		UserAgentTestCase{
+			UA:             "Mozilla/5.0 (iPhone; CPU iPhone OS 7_1_1 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D201 Safari/9537.53",
+			BrowserName:    "safari",
+			BrowserVersion: "7.0",
+			Engine:         "webkit",
+			EngineVersion:  "537.51.2",
+			OS:             "iPhone OS 7.1",
+			Platform:       "iphone",
+			Mobile:         true,
+		},
+
+		UserAgentTestCase{
+			UA:             "Mozilla/5.0 (iPhone; CPU iPhone OS 7_1 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D167 Safari/9537.53",
+			BrowserName:    "safari",
+			BrowserVersion: "7.0",
+			Engine:         "webkit",
+			EngineVersion:  "537.51.2",
+			OS:             "iPhone OS 7.1",
+			Platform:       "iphone",
+			Mobile:         true,
+		},
+
+		UserAgentTestCase{
+			UA:             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.73.11 (KHTML, like Gecko) Version/7.0.1 Safari/537.73.11",
+			BrowserName:    "safari",
+			BrowserVersion: "7.0.1",
+			Engine:         "webkit",
+			EngineVersion:  "537.73.11",
+			OS:             "OS X 10.9",
+			Platform:       "macintosh",
+			Mobile:         false,
+		},
+
+		UserAgentTestCase{
+			UA:             "Mozilla/5.0 (iPhone; CPU iPhone OS 7_1_2 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D257 Safari/9537.53",
+			BrowserName:    "safari",
+			BrowserVersion: "7.0",
+			Engine:         "webkit",
+			EngineVersion:  "537.51.2",
+			OS:             "iPhone OS 7.1",
+			Platform:       "iphone",
+			Mobile:         true,
+		},
+
+		UserAgentTestCase{
+			UA:             "Mozilla/5.0 (iPad; CPU OS 7_1_2 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D257 Safari/9537.53",
+			BrowserName:    "safari",
+			BrowserVersion: "7.0",
+			Engine:         "webkit",
+			EngineVersion:  "537.51.2",
+			OS:             "iPad OS 7.1",
+			Platform:       "ipad",
+			Mobile:         true,
+		},
+	}
+
+	for _, test := range testCases {
+		var ua = useragent.New(test.UA)
+
+		// browser name
+		if got := ua.BrowserName(); got != test.BrowserName {
+			t.Errorf("UserAgent.BrowserName[%s] => %s; want %s",
+				test.UA,
+				got,
+				test.BrowserName,
+			)
+		}
+
+		// browser version
+		if got := ua.BrowserVersion(); got != test.BrowserVersion {
+			t.Errorf("UserAgent.BrowserVersion[%s] => %s; want %s",
+				test.UA,
+				got,
+				test.BrowserVersion,
+			)
+		}
+
+		// engine
+		if got := ua.Engine(); got != test.Engine {
+			t.Errorf("UserAgent.Engine[%s] => %s; want %s",
+				test.UA,
+				got,
+				test.Engine,
+			)
+		}
+
+		// engine version
+		if got := ua.EngineVersion(); got != test.EngineVersion {
+			t.Errorf("UserAgent.EngineVersion[%s] => %s; want %s",
+				test.UA,
+				got,
+				test.EngineVersion,
+			)
+		}
+
+		// operating system
+		if got := ua.OS(); got != test.OS {
+			t.Errorf("UserAgent.OS[%s] => %s; want %s",
+				test.UA,
+				got,
+				test.OS,
+			)
+		}
+
+		// platform
+		if got := ua.Platform(); got != test.Platform {
+			t.Errorf("UserAgent.Platform[%s] => %s; want %s",
+				test.UA,
+				got,
+				test.Platform,
+			)
+		}
+
+		// mobile
+		if got := ua.Mobile(); got != test.Mobile {
+			t.Errorf("UserAgent.Mobile[%s] => %t; want %t",
+				test.UA,
+				got,
+				test.Mobile,
+			)
+		}
+
+	}
+}
