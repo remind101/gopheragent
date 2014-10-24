b8e623659e8af94446a243c314536fad80ce8816
Nathan Yergler <nathan@yergler.net>
2014-10-23 17:41:06 -0700

    Move testCase declaration to bottom of test file.

--- useragent_test.go
+++ useragent_test.go
@@ -17,9 +17,82 @@ type UserAgentTestCase struct {
 	Mobile bool
 }
 
+var testCases []UserAgentTestCase
+
 func Test_UserAgent_Parse(t *testing.T) {
 
-	testCases := []UserAgentTestCase{
+	for _, test := range testCases {
+		var ua = gopheragent.New(test.UA)
+
+		// browser name
+		if got := ua.BrowserName(); got != test.BrowserName {
+			t.Errorf("UserAgent.BrowserName[%s] => %s; want %s",
+				test.UA,
+				got,
+				test.BrowserName,
+			)
+		}
+
+		// browser version
+		if got := ua.BrowserVersion(); got != test.BrowserVersion {
+			t.Errorf("UserAgent.BrowserVersion[%s] => %s; want %s",
+				test.UA,
+				got,
+				test.BrowserVersion,
+			)
+		}
+
+		// engine
+		if got := ua.Engine(); got != test.Engine {
+			t.Errorf("UserAgent.Engine[%s] => %s; want %s",
+				test.UA,
+				got,
+				test.Engine,
+			)
+		}
+
+		// engine version
+		if got := ua.EngineVersion(); got != test.EngineVersion {
+			t.Errorf("UserAgent.EngineVersion[%s] => %s; want %s",
+				test.UA,
+				got,
+				test.EngineVersion,
+			)
+		}
+
+		// operating system
+		if got := ua.OS(); got != test.OS {
+			t.Errorf("UserAgent.OS[%s] => %s; want %s",
+				test.UA,
+				got,
+				test.OS,
+			)
+		}
+
+		// platform
+		if got := ua.Platform(); got != test.Platform {
+			t.Errorf("UserAgent.Platform[%s] => %s; want %s",
+				test.UA,
+				got,
+				test.Platform,
+			)
+		}
+
+		// mobile
+		if got := ua.Mobile(); got != test.Mobile {
+			t.Errorf("UserAgent.Mobile[%s] => %t; want %t",
+				test.UA,
+				got,
+				test.Mobile,
+			)
+		}
+
+	}
+}
+
+func init() {
+
+	testCases = []UserAgentTestCase{
 
 		UserAgentTestCase{
 			UA:             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/6.1.3 Safari/537.75.14",
@@ -132,71 +205,4 @@ func Test_UserAgent_Parse(t *testing.T) {
 		},
 	}
 
-	for _, test := range testCases {
-		var ua = gopheragent.New(test.UA)
-
-		// browser name
-		if got := ua.BrowserName(); got != test.BrowserName {
-			t.Errorf("UserAgent.BrowserName[%s] => %s; want %s",
-				test.UA,
-				got,
-				test.BrowserName,
-			)
-		}
-
-		// browser version
-		if got := ua.BrowserVersion(); got != test.BrowserVersion {
-			t.Errorf("UserAgent.BrowserVersion[%s] => %s; want %s",
-				test.UA,
-				got,
-				test.BrowserVersion,
-			)
-		}
-
-		// engine
-		if got := ua.Engine(); got != test.Engine {
-			t.Errorf("UserAgent.Engine[%s] => %s; want %s",
-				test.UA,
-				got,
-				test.Engine,
-			)
-		}
-
-		// engine version
-		if got := ua.EngineVersion(); got != test.EngineVersion {
-			t.Errorf("UserAgent.EngineVersion[%s] => %s; want %s",
-				test.UA,
-				got,
-				test.EngineVersion,
-			)
-		}
-
-		// operating system
-		if got := ua.OS(); got != test.OS {
-			t.Errorf("UserAgent.OS[%s] => %s; want %s",
-				test.UA,
-				got,
-				test.OS,
-			)
-		}
-
-		// platform
-		if got := ua.Platform(); got != test.Platform {
-			t.Errorf("UserAgent.Platform[%s] => %s; want %s",
-				test.UA,
-				got,
-				test.Platform,
-			)
-		}
-
-		// mobile
-		if got := ua.Mobile(); got != test.Mobile {
-			t.Errorf("UserAgent.Mobile[%s] => %t; want %t",
-				test.UA,
-				got,
-				test.Mobile,
-			)
-		}
-
-	}
 }
